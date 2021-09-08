7d5e4f623f299424413bf99b203d444e41ac2ccc
CanasDev <dg0837.dc@gmail.com>
2021-09-08 13:44:27 +0100

    Delete js directory

--- js/discordjs/verifyDM.js
+++
@@ -1,9 +0,0 @@
-// By Leonardo Machado and lint by atshushi
-function verifydm(user) {
-  return user.send('').catch((error) => error.message !== 'Cannot send messages to this user');
-}
-
-// Usage
-(async () => {
-  await verifydm(<user>); // If it is true then the person's private is turned on. If it is false, it is turned off
-})();
